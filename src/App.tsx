5accaaec8cbf54536712995fabd85355fdb082da
Wojciech Kochanowski <wojciech.kochanowski@o2.pl>
2023-04-30 13:47:03 +0200

    basic OAuth connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import './App.css'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import Login from './components/authentication/Login'
 
 function App() {
+  if (typeof process.env.REACT_APP_CLIENT_ID === 'undefined'){
+    return (<div>google client id not configured</div>)
+  }
   return (
-    <div className="App">
-      hello
-    </div>
-  );
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
+      <div className="App">
+        <Login/>
+      </div>
+    </GoogleOAuthProvider>
+  )
 }
 
 export default App;
