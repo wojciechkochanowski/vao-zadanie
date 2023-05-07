e9fa95f4510a2ca41103df6b160144437327dc6a
Wojciech Kochanowski <wojciech.kochanowski@o2.pl>
2023-05-07 19:15:01 +0200

    react-query select

--- src/hooks/useProjectDetails.ts
+++ src/hooks/useProjectDetails.ts
@@ -1,13 +1,11 @@
 import { useQuery } from 'react-query'
-import { TProject } from '../types/types'
 
 export default function useProjectDetails(id: string | undefined) {
   const { isLoading, data } = useQuery(['project_details', id], async () => {
     const res = await fetch(`${process.env.REACT_APP_API_URL}/projects?id=${id}`)
     return res.json()
+  }, {
+    select: data => data && data[0] || undefined
   })
-  let project
-  if(data)
-    project = (data as TProject[])[0]
-  return { isLoading, project }
+  return { isLoading, project: data }
 }
